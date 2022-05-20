737a3649f00fe91c67dc9d60ed9e881e2f42d472
Marly Diallo <marly.marlou@gmail.com>
2022-05-20 11:37:13 +0200

    watch list enhanced

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,35 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require "faker"
+# require "faker"
 
+# Movie.destroy_all
+
+# 20.times do
+#   movie = Movie.new(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#     poster_url: Faker::LoremFlickr.image(size: "500x500", search_terms: ['movie']),
+#     rating: rand(1.0..10.0)
+#   )
+#   movie.save!
+# end
+
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.open(url).read)
+
+puts 'Destroying all movies'
 Movie.destroy_all
 
-20.times do
-  movie = Movie.new(
-    title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
-    poster_url: Faker::LoremFlickr.image(size: "500x500", search_terms: ['movie']),
-    rating: rand(1.0..10.0)
-  )
-  movie.save!
+puts 'Creating movies...'
+movies["results"].each do |movie|
+    Movie.create(
+      title: movie["original_title"],
+      rating: movie["vote_average"],
+      overview: movie["overview"],
+      poster_url: movie["poster_path"]
+    )
 end
+
+puts 'All done'
